057c9192cbc0cd0ee84a1d86603bffe77d408072
Francesco Sblendorio <sblendorio@gmail.com>
2024-05-10 16:33:03 +0200

    add ET function

--- T.js
+++ T.js
@@ -5,6 +5,13 @@ function T(key, src) { // _T reserved keyword
 	T[key] = Function('ctx', t + 'return _T.T //# sourceURL=T.' + key)
 } // v 5.0 - 2021 giovanni tigli - paolo verri
 
+function ET(src, ctx) { // _T reserved keyword
+	let t = 'for(let _T in ctx)try{eval("var "+_T+"=ctx."+_T)}catch(e){}let _T={T:"",V:o=>o==null?"":o};'
+	for (let s of src.split(/\[([#@][\s\S]*?)#\]/g))
+		t += s.startsWith('@') ? '_T.T+=_T.V(' + s.slice(1).trim() + ');' : s.startsWith('#') ? s.slice(1) : '_T.T+=`' + s.replace(/`/g, '\\`') + '`;'
+	return Function('ctx', t + 'return _T.T')(ctx)
+}
+
 function Q(q) {
 	return document.querySelector(q)
 }
