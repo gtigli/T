a0d5619acbb8385522487c940bc0f44dbf54d248
Francesco Sblendorio <sblendorio@gmail.com>
2024-05-10 16:54:49 +0200

    default values

--- T.js
+++ T.js
@@ -1,8 +1,8 @@
-const T = new Proxy(function (src, val = {}, et = true) {
+const T = new Proxy(function (src = '', val = {}, et = true) {
 	let t = 'const _T={T:"",V:o=>o==null?"":o};'
 	for (let s of src.split(/\[([#@][\s\S]*?)#\]/g))
 		t += s.startsWith('@') ? '_T.T+=_T.V(' + s.slice(1).trim() + ');' : s.startsWith('#') ? s.slice(1) : '_T.T+=`' + s.replace(/`/g, '\\`') + '`;'
-	return et ? Function('ctx', t + 'return _T.T')(val) : Function('ctx', t + 'return _T.T //# sourceURL=T.' + val);
+	return et ? Function('ctx', t + 'return _T.T')(val) : Function('ctx = {}', t + 'return _T.T //# sourceURL=T.' + val);
 }, {
 	set(target, prop, value) {
 		target[prop] = target(value, prop, false);
